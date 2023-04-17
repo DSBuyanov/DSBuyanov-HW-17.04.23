054c86888c0b9bdad50da0c693b3ed66d291b2d2
DSBuyanov <inquis87@gmail.com>
2023-04-17 16:59:41 +0300

    task 1

---
+++ src/main/java/ru/geekbrains/task_01.java
@@ -0,0 +1,28 @@
+package ru.geekbrains;
+
+import java.util.Scanner;
+
+public class task_01 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Вычисляем n-ое треугольное число
+            System.out.print("Введите число n для вычисления n-го треугольного числа: ");
+            int n = scanner.nextInt();
+            int triangleNumber = 0;
+            for (int i = 1; i <= n; i++) {
+                triangleNumber += i;
+            }
+            System.out.println("n-ое треугольное число: " + triangleNumber);
+
+            // Вычисляем факториал числа n
+            System.out.print("Введите число n для вычисления факториала: ");
+            n = scanner.nextInt();
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            System.out.println("Факториал числа " + n + ": " + factorial);
+        }
+    }
+    
+}
